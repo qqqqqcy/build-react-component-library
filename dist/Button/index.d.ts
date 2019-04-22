3635161c2b7eba805ea6933467eda2869220fa54
unknown <qiuchiyi@360buyad.local>
2019-04-22 10:49:18 +0800

    修改

--- dist/Button/index.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './style/index.scss';
-interface Props extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-    value: string;
-}
-declare const Button: (props: Props) => JSX.Element;
-export default Button;
